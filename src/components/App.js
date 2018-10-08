cf8d254c1ddac02c2b8a4e4c64c127958d5c9f88
Bob Polacheck <rpolacheck@gmail.com>
2018-10-08 04:32:46 +0000

    added react, bootstrap, express and redux packages

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+         return (
+            <div>
+                <h2>Flashcard Pro</h2>
+            </div> 
+             
+        );
+    }   
+}
+
+export default App;
